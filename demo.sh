499886eaa75626c66a767939b28f2289df2213c4
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-18 20:16:49 +0530

    Changes to be committed: 	modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -1,28 +1,27 @@
-#!/bin/bash
 
-# Update package lists
+# sudo apt-get update
+
+# # Remove old package lists (optional)
+# sudo rm -fR /var/lib/apt/lists/*
+
+# # Update again to refresh package lists after removal (optional)
 sudo apt-get update
 
-# Remove old package lists (optional)
-sudo rm -fR /var/lib/apt/lists/*
-
-# Update again to refresh package lists after removal (optional)
-sudo apt update
-
-# Install prerequisites
+# # Install prerequisites
 sudo apt-get install -y python-software-properties software-properties-common
 
 # Remove the specific repository
-sudo add-apt-repository --remove -y ppa:alex-p/tesseract-ocr5
+sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
 
 # Update package lists after repository removal
 sudo apt-get update
 
 # Install Tesseract OCR
-sudo apt install -y tesseract-ocr tesseract-ocr-eng
+sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
 
 # Install Poppler utils without user interaction
 sudo apt-get install -y poppler-utils
+echo "Everything OK"
 
 # Display Tesseract version
 tesseract --version
