52f7a306136dc144463df184b6fbc2efe5b0050d
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-18 18:55:57 +0530

    Changes to be committed: 	modified:   .gitignore 	modified:   demo.sh 	new file:   requirements.txt

--- demo.sh
+++ demo.sh
@@ -9,11 +9,4 @@ sudo apt-get update
 tesseract --version
 sudo apt install poppler-utils
 sudo apt install libgl1-mesa-glx
-pip install pdf2image
-pip install langchain_community
-pip install -qU pytesseract
-pip install langchain-mistralai
-pip install opencv-python
-pip install python-dotenv
-pip install fastapi
-pip install uvicorn
+pip install -r requirements.txt
