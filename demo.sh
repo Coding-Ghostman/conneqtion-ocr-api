1099273846cabc6451787e5cb2e07c139b62d92a
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-18 18:52:42 +0530

    Changes to be committed: 	modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -1,17 +1,19 @@
-sudo add-apt-repository --remove ppa:alex-p/tesseract-ocr5
 sudo apt-get update
+sudo apt update
+sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:alex-p/tesseract-ocr5
 sudo apt install -y tesseract-ocr
 sudo apt-get install tesseract-ocr-eng
 sudo apt-get update
 tesseract --version
 sudo apt install poppler-utils
-sudo apt install python3-pip
 sudo apt install libgl1-mesa-glx
-pip3 install pdf2image
-pip3 install langchain_community
-pip3 install -qU pytesseract
-pip3 install langchain-mistralai
-pip3 install opencv-python
-pip3 install python-dotenv
-pip3 install fastapi
-pip3 install uvicorn
+pip install pdf2image
+pip install langchain_community
+pip install -qU pytesseract
+pip install langchain-mistralai
+pip install opencv-python
+pip install python-dotenv
+pip install fastapi
+pip install uvicorn
