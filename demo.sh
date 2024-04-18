25098bbefa081ab6408cf0183223214e55188987
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-18 20:03:29 +0530

    Changes to be committed: 	modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -3,11 +3,11 @@ sudo rm -fR /var/lib/apt/lists/*
 sudo apt update
 sudo apt-get install python-software-properties
 sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:alex-p/tesseract-ocr5
+sudo add-apt-repository --remove ppa:alex-p/tesseract-ocr5
 sudo apt-get update
 sudo apt install -y tesseract-ocr
 sudo apt-get install tesseract-ocr-eng
 sudo apt-get update
-echo tesseract --version
 sudo apt install poppler-utils
-sudo apt install libgl1-mesa-glx
+echo tesseract --version
+tesseract --version
