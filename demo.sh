76041001d65695fbf64e7a09d5b9b6ea66af4046
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-19 12:12:47 +0530

    Changes to be committed: 	modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -1,11 +1,6 @@
 
-# sudo apt-get update
-
-# # Remove old package lists (optional)
-# sudo rm -fR /var/lib/apt/lists/*
-
-# # Update again to refresh package lists after removal (optional)
 sudo apt-get update
+pip install --upgrade build setuptools
 
 # # Install prerequisites
 sudo apt-get install -y python-software-properties software-properties-common
@@ -15,19 +10,15 @@ sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
 
 # Update package lists after repository removal
 sudo apt-get update
-sudo apt-get install -y libc6
 sudo apt-get install -y ffmpeg libsm6 libxext6
 sudo apt-get install -y libgl1
 sudo apt-get install -y opencv-python-headless
-# Install Tesseract OCR
 sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
-
-# Install Poppler utils without user interaction
 sudo apt-get install -y poppler-utils
 sudo apt-get install -y libgl1-mesa-glx
+sudo apt-get install -y libc6
+nix-shell -p GLIBC-2.36
 mkdir converted_images
 ls
 echo "Everything OK"
-
-# # Display Tesseract version
-# tesseract --version
+tesseract --version
