70a64683647716733046bfb16273f851b323daee
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-19 11:29:59 +0530

    Changes to be committed: 	modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -16,7 +16,7 @@ sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
 # Update package lists after repository removal
 sudo apt-get update
 sudo apt-get install -y libc6
-sudo apt-get install -y ffmpeg libsm6 libxext6
+# sudo apt-get install -y ffmpeg libsm6 libxext6
 sudo apt-get install -y libgl1
 # sudo apt-get install -y python3-opencv
 # Install Tesseract OCR
