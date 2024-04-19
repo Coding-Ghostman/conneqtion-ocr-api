b5c741bbca741ba9cadce6181c015f16b88e3134
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-19 15:24:36 +0530

    Changes to be committed: 	modified:   main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 sudo apt-get update
-pip install -qU build setuptools
 
 sudo apt-get install software-properties-common
 sudo add-apt-repository --remove ppa:alex-p/tesseract-ocr
 sudo apt-get update
 
+sudo apt-get install -y libc6
 sudo apt-get install -y tesseract-ocr 
 sudo apt-get install libtesseract-dev
 sudo apt-get install -y poppler-utils
