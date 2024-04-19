0ca870d8a57f2c40080e451c2fec32a30f839a8e
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-19 12:57:48 +0530

    modified:   demo.sh

--- demo.sh
+++ demo.sh
@@ -4,6 +4,9 @@ pip install --upgrade build setuptools
 
 # # Install prerequisites
 sudo apt-get install -y python-software-properties software-properties-common
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install gcc-4.9
+sudo apt-get upgrade libstdc++6
 
 # Remove the specific repository
 sudo add-apt-repository -y ppa:alex-p/tesseract-ocr5
@@ -17,8 +20,16 @@ sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
 sudo apt-get install -y poppler-utils
 sudo apt-get install -y libgl1-mesa-glx
 sudo apt-get install -y libc6
-sudo apt-get install -y GLIBC-2.36
-nix-shell -p iconv -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/e89cf1c932006531f454de7d652163a9a5c86668.tar.gz
+
+exho "-------------------------------------------------"
+echo "NIX_ENV"
+exho "-------------------------------------------------"
+nix-env -iA iconv -f https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
+exho "-------------------------------------------------"
+echo "NIX_SHELL"
+exho "-------------------------------------------------"
+nix-shell -p iconv -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/9957cd48326fe8dbd52fdc50dd2502307f188b0d.tar.gz
+
 mkdir converted_images
 ls
 echo "Everything OK"
