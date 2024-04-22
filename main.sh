12bf483a887a89e12fc7950ce6a5000a634231d4
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-22 13:59:53 +0530

    Update nixpacks.toml with additional setup commands and glibc-2.36 configure command

--- main.sh
+++ main.sh
@@ -1,3 +1,17 @@
 
 echo "Everything OK"
 tesseract --version
+
+
+  'sudo apt-get install -y devscripts build-essential gawk bison texinfo',
+  'echo "Everything OK ----------------------------------"',
+  'mkdir $HOME/glibc/ && cd $HOME/glibc',
+  'wget http://ftp.gnu.org/gnu/libc/glibc-2.36.tar.gz',
+  'tar -xvzf glibc-2.36.tar.gz',
+  'mkdir build',
+  'mkdir glibc-2.36-install',
+  'ls',
+  'cd build',
+  './glibc-2.36/configure --prefix=/glibc-2.36-install',
+  'make',
+  'make install',
