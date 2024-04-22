bec25f52d82df55b0bee6f04d09495a592999533
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-22 12:47:35 +0530

    Changes to be committed: 	modified:   main.sh 	new file:   nixpacks.toml

--- main.sh
+++ main.sh
@@ -5,6 +5,8 @@ sudo add-apt-repository --remove ppa:alex-p/tesseract-ocr
 sudo apt-get update
 
 sudo apt-get install -y libc6
+sudo apt-get install -y ffmpeg libsm6 libxext6 libc6
+
 sudo apt-get install -y tesseract-ocr 
 sudo apt-get install libtesseract-dev
 sudo apt-get install -y poppler-utils
