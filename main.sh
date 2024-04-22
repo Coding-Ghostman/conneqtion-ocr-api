de844190d89d8e9c9e50bb80a1318ab663e2984e
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-22 12:48:25 +0530

    Changes to be committed: 	modified:   main.sh

--- main.sh
+++ main.sh
@@ -1,17 +1,3 @@
-sudo apt-get update
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository --remove ppa:alex-p/tesseract-ocr
-sudo apt-get update
-
-sudo apt-get install -y libc6
-sudo apt-get install -y ffmpeg libsm6 libxext6 libc6
-
-sudo apt-get install -y tesseract-ocr 
-sudo apt-get install libtesseract-dev
-sudo apt-get install -y poppler-utils
-
-
-mkdir converted_images
 echo "Everything OK"
 tesseract --version
