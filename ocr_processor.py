5238cc8b92a23ec6a73f57422ed3d00ec3a6b716
Coding-Ghostman <atmanmishra1122@gmail.com>
2024-04-26 17:36:09 +0530

    Changes to be committed: 	modified:   deletor.py 	modified:   ocr_processor.py

--- ocr_processor.py
+++ ocr_processor.py
@@ -1,5 +1,4 @@
 import re, json, os
-from logger import setup_logger
 from deletor import delete_log_file, delete_files_in_directory
 from llm_checker import get_llm_help, grammar_corrector
 from pdf_extractor import convert_pdf_img, extract_all_data
